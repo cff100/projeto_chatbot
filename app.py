59b8c7ffd39aa6fc5c6ffa3ffb6dbabb38debd6c
cff100 <caiofelix190799@gmail.com>
2026-06-25 10:13:19 -0300

    Pequenas modificações

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ with st.sidebar:
 
 # Título principal da página central
 st.title("📊 Chatbot Inteligente de Dados")
-st.markdown(f"Atualizado em: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}") 
+st.markdown(f"Backend atualizado em: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}") 
 
 
 # ==========================================
